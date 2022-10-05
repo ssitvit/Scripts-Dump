293c20a54099c9938b689926767edd5b95f0b101
Dhairya Ostwal <50984984+dhairyaostwal@users.noreply.github.com>
2022-10-05 23:22:31 +0530

    Create script.py

---
+++ VTOP-File-Rename/script.py
@@ -0,0 +1,24 @@
+import os
+import re
+import shutil
+
+# Input the destination
+destination = '/Users/dhairyaostwal/Downloads/'
+
+# In Windows = D:\\aayushi\\Documents\\someFolder
+directoryList = os.chdir(destination)
+print(os.getcwd())
+
+files = os.listdir()
+pattern = re.compile(r"\d\d-\d\d-20\d\d_(.+)$")
+
+for f in [file for file in files if os.path.isfile(file)]:
+	x=pattern.search(f)
+	name=f
+	if x:
+		name=x.group(0)
+		name=re.sub(r"_"," ",name)
+		name=re.sub(r"  +"," ",name)
+		shutil.move(os.path.join(os.getcwd(),f),os.path.join(os.getcwd(),name))
+		
+print("Success :)")
